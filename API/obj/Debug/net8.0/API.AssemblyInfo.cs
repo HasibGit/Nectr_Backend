f6260e17a7f73cbd95478cf5440342207e03dda8
u1176456 <mdhasib.ullah@iqvia.com>
2025-04-10 23:53:32 +0600

    cleaned up appsettings and program cs file

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcbcafbd99515632139aa586429a5f2b9f1035e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc9e9187ac25e420c177893c3d788c205fe3ab12")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
