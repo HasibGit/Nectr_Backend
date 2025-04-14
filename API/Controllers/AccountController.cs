204420277736fbc5da176b9caca25d1a63e978e3
u1176456 <mdhasib.ullah@iqvia.com>
2025-04-15 02:22:50 +0600

    updated entity, added new controller for registering users

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController(DataContext context) : BaseApiController
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.UserName))
+            {
+                return BadRequest("User name already exists!");
+            }
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDto.UserName,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key,
+            };
+
+            context.Users.Add(user);
+
+            await context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        private async Task<bool> UserExists(string userName)
+        {
+            return await context.Users.AnyAsync(user => user.UserName.ToLower() == userName.ToLower());
+        }
+    }
+}
