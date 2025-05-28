aa948716b81c9ae2d001383dcfcccf2e77de257d
u1176456 <mdhasib.ullah@iqvia.com>
2025-05-28 22:54:46 +0600

    API added to update user info

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -10,7 +11,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers
 {
     [Authorize]
-    public class UsersController(IUserRepository userRepository) : BaseApiController
+    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -31,6 +32,33 @@ namespace API.Controllers
             }
 
             return Ok(user);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (username is null)
+            {
+                return BadRequest();
+            }
+            
+            var user = await userRepository.GetUserByUsernameAsync(username);
+
+            if (user is null)
+            {
+                return NotFound("User not found");
+            }
+            
+            mapper.Map(memberUpdateDto, user);
+
+            if (await userRepository.SaveAllAsync())
+            {
+                return NoContent();
+            }
+            
+            return BadRequest("Failed to update user");
         }
     }
 }
