eaba1bb85dab41469eff55730bc8dda9a746cc7d
u1176456 <mdhasib.ullah@iqvia.com>
2025-06-12 21:48:35 +0600

    working on register user endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -13,7 +13,7 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            if (await UserExists(registerDto.UserName))
+            if (await UserExists(registerDto.Username))
             {
                 return BadRequest("User name already exists!");
             }
--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,7 +6,30 @@ namespace API.DTOs;
 public class RegisterDto
 {
     [Required]
-    public required string UserName { get; set; }
+    public required string Gender { get; set; }
+
     [Required]
+    public required string Username { get; set; }
+
+    [Required]
+    public required string KnownAs { get; set; }
+
+    [Required]
+    public required DateOnly DateOfBirth { get; set; }
+
+    [Required]
+    public required string City { get; set; }
+
+    [Required]
+    public required string Country { get; set; }
+
+    [Required]
+    [MinLength(8)]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$", 
+        ErrorMessage = "Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character.")]
     public required string Password { get; set; }
+
+    [Required]
+    [Compare("Password", ErrorMessage = "Passwords do not match.")]
+    public required string ConfirmPassword { get; set; }
 }
