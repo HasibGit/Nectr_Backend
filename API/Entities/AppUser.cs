9c8cffe5106e30bed0c71894b0c388b0a012496a
u1176456 <mdhasib.ullah@iqvia.com>
2025-08-09 15:56:55 +0600

    added Message Entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -22,4 +22,6 @@ public class AppUser
     public List<Photo> Photos { get; set; } = [];
     public List<Like> Likes { get; set; } = [];
     public List<Like> LikedBy { get; set; } = [];
+    public List<Message> MessagesSent { get; set; } = [];
+    public List<Message> MessagesReceived { get; set; } = [];
 }
