6de97668fc97cd4528a550934a1552994b8bfe7a
u1176456 <mdhasib.ullah@iqvia.com>
2025-08-23 21:40:01 +0600

    updated seed data

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -2,15 +2,16 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
 public class Seed
 {
-    public static async Task SeedUsers(DataContext context)
+    public static async Task SeedUsers(UserManager<AppUser> userManager)
     {
-        if (await context.Users.AnyAsync())
+        if (await userManager.Users.AnyAsync())
         {
             return;
         }
@@ -26,11 +27,7 @@ public class Seed
 
         foreach (var user in users)
         {
-            using var hmac = new HMACSHA512();
-
-            context.Users.Add(user);
+            await userManager.CreateAsync(user, "minFiCo34$%");
         }
-
-        await context.SaveChangesAsync();
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
 using API.Data;
+using API.Entities;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,13 +28,14 @@ var services = scope.ServiceProvider;
 try
 {
   var context = services.GetRequiredService<DataContext>();
+  var userManager = services.GetRequiredService<UserManager<AppUser>>();
   await context.Database.MigrateAsync();
-  await Seed.SeedUsers(context);
+  await Seed.SeedUsers(userManager);
 }
 catch (Exception ex)
 {
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex.Message, "An error occured during migration");
+  var logger = services.GetRequiredService<ILogger<Program>>();
+  logger.LogError(ex.Message, "An error occured during migration");
 }
 
 app.Run();
