84e805dc20c37c22511b03ffb5e0bbb6a323ee57
u1176456 <mdhasib.ullah@iqvia.com>
2025-08-29 14:56:48 +0600

    role update endpoint implemented

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,15 +1,63 @@
+using API.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AdminController : BaseApiController
+    public class AdminController(UserManager<AppUser> userManager) : BaseApiController
     {
         [Authorize(Policy = "RequireAdminRole")]
         [HttpGet("users-with-roles")]
-        public ActionResult GetUsersWithRoles()
+        public async Task<ActionResult> GetUsersWithRoles()
         {
-            return Ok("Only admins can see this.");
+            var users = await userManager.Users
+                                        .OrderBy(x => x.UserName)
+                                        .Select(x => new
+                                        {
+                                            x.Id,
+                                            x.UserName,
+                                            Roles = x.UserRoles.Select(r => r.Role.Name).ToList()
+                                        }).ToListAsync();
+
+            return Ok(users);
+        }
+
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpPost("edit-roles/{username}")]
+        public async Task<ActionResult> EditRoles(string username, string roles)
+        {
+            if (string.IsNullOrEmpty(roles))
+            {
+                return BadRequest("No roles selected");
+            }
+
+            var selectedRoles = roles.Split(",").ToArray();
+            var user = await userManager.FindByNameAsync(username);
+
+            if (user is null)
+            {
+                return BadRequest("User not found");
+            }
+
+            var userRoles = await userManager.GetRolesAsync(user);
+
+            var result = await userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+            if (!result.Succeeded)
+            {
+                return BadRequest("Role assignment failed");
+            }
+
+            result = await userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if (!result.Succeeded)
+            {
+                return BadRequest("Role assignment failed");
+            }
+
+            return Ok(await userManager.GetRolesAsync(user));
         }
 
         [Authorize(Policy = "ModeratePhotoRole")]
