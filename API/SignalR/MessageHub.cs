20df361e51dd43b2e23394f52419f5a936396201
u1176456 <mdhasib.ullah@iqvia.com>
2025-09-11 15:16:26 +0600

    working in integrating signalR for messaging feature

--- API/SignalR/MessageHub.cs
+++ API/SignalR/MessageHub.cs
@@ -1,12 +1,15 @@
+using API.DTOs;
+using API.Entities;
 using API.Extensions;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace API.SignalR;
 
 [Authorize]
-public class MessageHub(IMessageRepository messageRepository) : Hub
+public class MessageHub(IMessageRepository messageRepository, IUserRepository userRepository, IMapper mapper) : Hub
 {
     public override async Task OnConnectedAsync()
     {
@@ -30,6 +33,44 @@ public class MessageHub(IMessageRepository messageRepository) : Hub
     public override Task OnDisconnectedAsync(Exception? exception)
     {
         return base.OnDisconnectedAsync(exception);
+    }
+
+    public async Task SendMessage(CreateMessageDto createMessageDto)
+    {
+        var username = Context.User?.GetUserName() ?? throw new HubException("User not found");
+
+        if (username == createMessageDto.RecipientUsername.ToLower())
+        {
+            throw new HubException("You cannot message yourself");
+        }
+
+        var sender = await userRepository.GetUserByUsernameAsync(username);
+        var recipient = await userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+        if (recipient == null || sender == null || sender.UserName == null || recipient.UserName == null)
+        {
+            throw new HubException("Message sending failed");
+        }
+
+        var message = new Message
+        {
+            Sender = sender,
+            Recipient = recipient,
+            SenderUsername = sender.UserName,
+            RecipientUsername = recipient.UserName,
+            Content = createMessageDto.Content
+        };
+
+        messageRepository.AddMessage(message);
+
+        if (await messageRepository.SaveAllAsync())
+        {
+            var group = GetGroupName(sender.UserName, recipient.UserName);
+
+            await Clients.Group(group).SendAsync("NewMessage", mapper.Map<MessageDto>(message));
+        }
+
+        throw new HubException("Message sending failed");
     }
 
     private string GetGroupName(string sender, string receiver)
